dea34496ec74669d7f9a816be44163d679328f86
kuldeep  jeengar <kuldeepjeengar50@gmail.com>
2024-05-19 12:09:11 +0530

    update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name= 'mcqgenrator',
+    version= '0.0.1',
+    author='kuldeep jeengar',
+    author_email='kuldeepjeengar50@gmail.com',
+    install_requires=["openai", "langchain","streamlit", "python-dotenv", "PyPDF2" ],
+    packages=find_packages()
+)
